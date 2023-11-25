52be949b1c02bee65ac71e18efdd9dc189cb3092
Muhammad Fajri Assidiq <dev.muhafars@gmail.com>
2023-11-25 20:51:39 +0700

    install dotenv as environment variable

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 import products from "./data/products.js";
-const port = 3002;
+const port = process.env.PORT || 3002;
 const app = express();
 
 app.get("/", (req, res) => {
