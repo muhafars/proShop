1dd0a724ccd206e0da04daa0e4671129f7e8091c
Muhammad Fajri Assidiq <dev.muhafars@gmail.com>
2023-11-30 02:34:57 +0700

    add cartSlice and initialize it at store.js(as root of redux) check redux toolkit to see state cart

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,11 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/apiSlices";
+import cartSliceReducer from "./slices/cartSlice";
 
 const stores = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
+    cart: cartSliceReducer,
   },
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true,
